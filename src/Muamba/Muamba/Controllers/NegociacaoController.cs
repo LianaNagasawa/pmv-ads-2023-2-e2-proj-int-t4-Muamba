b79595bb12af6f5841240c6052b59e8a5aa6f508
LianaNagasawa <lcvsnagasawa@sga.pucminas.br>
2023-10-05 23:55:19 -0300

    Estrutura Básica Site Muamba

---
+++ src/Muamba/Muamba/Controllers/NegociacaoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Muamba.Controllers
+{
+    public class NegociacaoController : Controller
+    {
+        public IActionResult Avaliacoes()
+        {
+            return View();
+        }
+        public IActionResult IniciarNegociacao()
+        {
+            return View();
+        }
+        public IActionResult Negociacoes()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Muamba/Muamba/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Muamba.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscarProduto()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+        public IActionResult AnunciarViagem()
+        {
+            return View();
+        }
+    }
+}
