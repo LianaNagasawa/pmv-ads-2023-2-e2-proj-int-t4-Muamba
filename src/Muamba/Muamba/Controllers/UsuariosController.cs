b2e34dcb9a86fd9b6b5f80be76d1fbf73911f393
DouglasHMFe <dhmferreira@sga.pucminas.br>
2023-12-03 14:02:40 -0300

    Redirecionar Cadastro + CSS

--- src/Muamba/Muamba/Controllers/UsuariosController.cs
+++ src/Muamba/Muamba/Controllers/UsuariosController.cs
@@ -122,7 +122,7 @@ namespace Muamba.Controllers
                 usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index", "Home");
             }
             return View(usuario);
         }
